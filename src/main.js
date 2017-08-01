afbe15ee1370e41a283ef44c5051be3fa4d69be8
Dreampie <Dreampie@outlook.com>
2017-08-01 18:28:08 +0800

    update vue plugin

--- src/main.js
+++ src/main.js
@@ -4,12 +4,6 @@ import router from "./router.js";
 import App from "./App.vue";
 import properties from "./../application.json";
 
-Vue.use(Vue2Plugin)
-
-console.log(Vue.cookie)
-console.log(Vue.bus)
-console.log(Vue.http.defaults)
-
 console.log("App start env is: " + process.env.NODE_ENV)
 console.log("App root url is: " + properties[process.env.NODE_ENV].rootUrl)
 console.log("Api root url is: " + properties[process.env.NODE_ENV].apiRootUrl)
@@ -18,6 +12,13 @@ console.log("Login url is: " + properties[process.env.NODE_ENV].loginUrl)
 window.localStorage.setItem("rootUrl", properties[process.env.NODE_ENV].rootUrl)
 window.localStorage.setItem("apiRootUrl", properties[process.env.NODE_ENV].apiRootUrl)
 window.localStorage.setItem("loginUrl", properties[process.env.NODE_ENV].loginUrl)
+
+
+Vue.use(Vue2Plugin)
+
+console.log(Vue.cookie)
+console.log(Vue.bus)
+console.log(Vue.http.defaults)
 
 new Vue({
     router,
